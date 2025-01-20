86c4e74964361130e67a5cb625e047e5000fabda
Ignacio Amaya <iamaya@coopelectric.local>
2025-01-20 09:00:59 -0300

    cambios

--- public/scripts/login-admin.js
+++ public/scripts/login-admin.js
@@ -3,7 +3,8 @@ import { getAuth, signInWithEmailAndPassword, signOut } from "https://www.gstati
 
 const auth = getAuth(app);
 
-const adminEmail = "soutrelleagustin64@gmail.com"; // Correo del administrador
+const adminEmail = "soutrelleagustin64@gmail.com"; "naa87.1993@gmail.com"// Correo del administrador
+
 
 document.getElementById("admin-login-form").addEventListener("submit", async (e) => {
     e.preventDefault();
