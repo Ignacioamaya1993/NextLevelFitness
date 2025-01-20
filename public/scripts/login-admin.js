4d26e7ca03a066d6fa33ffc8ff8afc6cfedbf7bc
Ignacio Amaya <iamaya@coopelectric.local>
2025-01-20 08:55:25 -0300

    cambios

--- public/scripts/login-admin.js
+++ public/scripts/login-admin.js
@@ -1,21 +1,26 @@
+import app from "../scripts/firebaseConfig.js"; 
+import { getAuth, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
+
+const auth = getAuth(app);
+
 const adminEmail = "soutrelleagustin64@gmail.com"; // Correo del administrador
-const adminPassword = "adminasoutrelle";  // Contraseña del administrador
 
 document.getElementById("admin-login-form").addEventListener("submit", async (e) => {
     e.preventDefault();
     
-    const email = document.getElementById("admin-email").value;
-    const password = document.getElementById("admin-password").value;
+    const email = document.getElementById("admin-email").value.trim();
+    const password = document.getElementById("admin-password").value.trim();
 
     try {
-        const userCredential = await firebase.auth().signInWithEmailAndPassword(email, password);
+        const userCredential = await signInWithEmailAndPassword(auth, email, password);
         const user = userCredential.user;
 
         if (user.email === adminEmail) {
-            window.location.href = "panel-admin.html"; // Redirige al panel de administración
+            // Redirige al panel de administración
+            window.location.href = "panel-admin.html";
         } else {
             document.getElementById("login-error").innerText = "Acceso denegado. No eres administrador.";
-            firebase.auth().signOut(); // Cierra sesión si no es admin
+            await signOut(auth); // Cierra sesión si no es admin
         }
     } catch (error) {
         document.getElementById("login-error").innerText = "Error al iniciar sesión: " + error.message;
