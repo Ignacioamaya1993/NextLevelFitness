0a497ce329da3d45941c804b39bc2c36f8e763d8
Ignacio Amaya <you@example.com>
2025-01-30 10:46:24 -0300

    cambios

--- public/scripts/panel-admin.js
+++ public/scripts/panel-admin.js
@@ -1,6 +1,13 @@
+// Importar los módulos necesarios desde Firebase
+import { getAuth, signOut } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
+import app from "../scripts/firebaseConfig.js"; // Asegúrate de que esta importación esté correcta
+
 // Cerrar sesión
 document.getElementById("logout").addEventListener("click", () => {
-    firebase.auth().signOut().then(() => {
-        window.location.href = "login-admin.html";
+    const auth = getAuth(app);  // Usamos la instancia de autenticación
+    signOut(auth).then(() => {
+        window.location.href = "login-admin.html"; // Redirigir al login
+    }).catch((error) => {
+        console.error("Error al cerrar sesión:", error);
     });
 });
